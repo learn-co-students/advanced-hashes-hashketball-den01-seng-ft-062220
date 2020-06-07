91fcbb90647f97090ed0c05ce43fee062417e349
Hyung Kim <hjkmines@gmail.com>
2020-06-07 22:06:36 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,129 +1,144 @@
 # Write your code below game_hash
+require "pry"
+
 def game_hash
-  {
-    home: {
-      team_name: "Brooklyn Nets",
-      colors: ["Black", "White"],
-      players: [
-        {
-          player_name: "Alan Anderson",
-          number: 0,
-          shoe: 16,
-          points: 22,
-          rebounds: 12,
-          assists: 12,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 1
-        },
-        {
-          player_name: "Reggie Evans",
-          number: 30,
-          shoe: 14,
-          points: 12,
-          rebounds: 12,
-          assists: 12,
-          steals: 12,
-          blocks: 12,
-          slam_dunks: 7
-        },
-        {
-          player_name: "Brook Lopez",
-          number: 11,
-          shoe: 17,
-          points: 17,
-          rebounds: 19,
-          assists: 10,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 15
-        },
-        {
-          player_name: "Mason Plumlee",
-          number: 1,
-          shoe: 19,
-          points: 26,
-          rebounds: 11,
-          assists: 6,
-          steals: 3,
-          blocks: 8,
-          slam_dunks: 5
-        },
-        {
-          player_name: "Jason Terry",
-          number: 31,
-          shoe: 15,
-          points: 19,
-          rebounds: 2,
-          assists: 2,
-          steals: 4,
-          blocks: 11,
-          slam_dunks: 1
-        }
-      ]
-    },
-    away: {
-      team_name: "Charlotte Hornets",
-      colors: ["Turquoise", "Purple"],
-      players: [
-        {
-          player_name: "Jeff Adrien",
-          number: 4,
-          shoe: 18,
-          points: 10,
-          rebounds: 1,
-          assists: 1,
-          steals: 2,
-          blocks: 7,
-          slam_dunks: 2
-        },
-        {
-          player_name: "Bismack Biyombo",
-          number: 0,
-          shoe: 16,
-          points: 12,
-          rebounds: 4,
-          assists: 7,
-          steals: 22,
-          blocks: 15,
-          slam_dunks: 10
-        },
-        {
-          player_name: "DeSagna Diop",
-          number: 2,
-          shoe: 14,
-          points: 24,
-          rebounds: 12,
-          assists: 12,
-          steals: 4,
-          blocks: 5,
-          slam_dunks: 5
-        },
-        {
-          player_name: "Ben Gordon",
-          number: 8,
-          shoe: 15,
-          points: 33,
-          rebounds: 3,
-          assists: 2,
-          steals: 1,
-          blocks: 1,
-          slam_dunks: 0
-        },
-        {
-          player_name: "Kemba Walker",
-          number: 33,
-          shoe: 15,
-          points: 6,
-          rebounds: 12,
-          assists: 12,
-          steals: 7,
-          blocks: 5,
-          slam_dunks: 12
-        }
-      ]
-    }
-  }
+
+  final_result = Hash.new
+  top_level = [:home, :away]
+  middle_level = [:team_name, :colors, :players]
+  bottom_level = [:player_name, :number, :shoe, :points, :rebounds, :assists, :steals, :blocks, :slam_dunks]
+  player_hash = Hash.new
+  home_players_array = []
+  away_players_array = []
+
+  #Home_Team
+  home_team_name = "Brooklyn Nets"
+  home_team_colors = ["Black", "White"]
+  home_player_1 = ["Alan Anderson", 0, 16, 22, 12, 12, 3, 1, 1]
+  home_player_2 = ["Reggie Evans", 30, 14, 12, 12, 12, 12, 12, 7]
+  home_player_3 = ["Brook Lopez", 11, 17, 17, 19, 10, 3, 1, 15]
+  home_player_4 = ["Mason Plumlee", 1, 19, 26, 11, 6, 3, 8, 5]
+  home_player_5 = ["Jason Terry", 31, 15, 19, 2, 2, 4, 11, 1]
+
+  #Away_Team
+  away_team_name = "Charlotte Hornets"
+  away_team_colors = ["Turquoise", "Purple"]
+  away_player_1 = ["Jeff Adrien", 4, 18, 10, 1, 1, 2, 7, 2]
+  away_player_2 = ["Bismack Biyombo", 0, 16, 12, 4, 7, 22, 15, 10]
+  away_player_3 = ["DeSagna Diop", 2, 14, 24, 12, 12, 4, 5, 5]
+  away_player_4 = ["Ben Gordon", 8, 15, 33, 3, 2, 1, 1, 0]
+  away_player_5 = ["Kemba Walker", 33, 15, 6, 12, 12, 7, 5, 12]
+
+#home players stats
+home_player_1_stats = bottom_level.zip(home_player_1).to_h
+home_player_2_stats = bottom_level.zip(home_player_2).to_h
+home_player_3_stats = bottom_level.zip(home_player_3).to_h
+home_player_4_stats = bottom_level.zip(home_player_4).to_h
+home_player_5_stats = bottom_level.zip(home_player_5).to_h
+
+home_players_array.push(home_player_1_stats, home_player_2_stats, home_player_3_stats, home_player_4_stats, home_player_5_stats)
+
+#away players stats
+away_player_1_stats = bottom_level.zip(away_player_1).to_h
+away_player_2_stats = bottom_level.zip(away_player_2).to_h
+away_player_3_stats = bottom_level.zip(away_player_3).to_h
+away_player_4_stats = bottom_level.zip(away_player_4).to_h
+away_player_5_stats = bottom_level.zip(away_player_5).to_h
+
+away_players_array.push(away_player_1_stats, away_player_2_stats, away_player_3_stats, away_player_4_stats, away_player_5_stats)
+
+  top_level.each do |x|
+    final_result[x] = {middle_level[0] => "", middle_level[1] => "", middle_level[2] => ""}
+  end
+
+#final output should look like
+  final_result.each do |k, v|
+    if k == :home
+      final_result[k][:team_name] = home_team_name
+      final_result[k][:colors] = home_team_colors
+      final_result[k][:players] = home_players_array
+    elsif k == :away
+      final_result[k][:team_name] = away_team_name
+      final_result[k][:colors] = away_team_colors
+      final_result[k][:players] = away_players_array
+    end
+  end
+  final_result
 end
 
-# Write code here
+def num_points_scored(player)
+
+game_hash.each do |k, v|
+  v[:players].each do |x|
+    if x[:player_name] == player
+      return x[:points]
+    end
+  end
+end
+end
+
+def shoe_size(player)
+
+  game_hash.each do |k, v|
+    v[:players].each do |x|
+      if x[:player_name] == player
+        return x[:shoe]
+      end
+    end
+  end
+end
+
+def team_colors(team)
+
+  game_hash.each do |k, v|
+    if v[:team_name] == team
+      return v[:colors]
+    end
+  end
+end
+
+def team_names
+teams = Array.new
+
+game_hash.each do |k, v|
+  teams.push(v[:team_name])
+end
+teams
+end
+
+def player_numbers(team)
+
+players_num = Array.new
+
+game_hash.each do |k, v|
+  v[:players].each do |x|
+    if v[:team_name] ==  team
+      players_num.push(x[:number])
+    end
+  end
+end
+players_num
+end
+
+def player_stats(player)
+
+game_hash.each do |k, v|
+  v[:players].each do |x|
+    if x[:player_name] == player
+      return x
+    end
+  end
+end
+
+end
+
+def big_shoe_rebounds
+
+game_hash.each do |k, v|
+  v[:players].each do |x|
+    if x[:shoe] == 19
+      return x[:rebounds]
+    end 
+  end
+end
+end
